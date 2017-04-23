f15721d5797f8dba3d13706b056bc7d15cfa1ce8
zhuxiang555111 <zhuxiang555111@hotmail.com>
2017-04-23 16:00:56 +0800

    verson 1.0.0

--- 1004.Counting leaves(30)/1004.Counting leaves(30)/main.cpp
+++ 1004.Counting leaves(30)/1004.Counting leaves(30)/main.cpp
@@ -1,58 +1,44 @@
-#pragma once
-#include <iostream>
+#include <cstdio>
 #include <vector>
-#include<stack>
+#include <algorithm>
 
 using namespace std;
 
-stack<int> sta; //empty stack
 
-void dfs(int array[][100])
-{
-	int i, j;
-	for (i = 0; i < 100; i++)
-	{
-		for (j = 0; j < 100; j++)
-		{
-			if (array[i][j] == 1)
-			{
-				sta.push(i);
+vector<int> v[100]; //看作邻接表形式存图
 
-			}
-		}
+
+int book[100], maxdepth = -1;
+
+
+void dfs(int index, int depth) {
+	if (v[index].size() == 0) {
+		book[depth]++;
+		maxdepth = max(maxdepth, depth);
+		return;
 	}
+	for (int i = 0; i < v[index].size(); i++)
+		dfs(v[index][i], depth + 1);
 }
 
-int main()
-{
-	int n, //number of nodes
-		m, //number of non-leaf nodes   非叶节点
-		k; //非叶的儿子个数
-	int id[100][100];
-	//100*100 vector二维数组
-	for (int a = 0; a < 100; a++)  //initial
-	{
-		for (int b = 0; b < 100; b++)
-		{
-			id[a][b] = 0;
-		}
-	}
-	scanf_s("%d %d", &m, &m);
-	for (int i = 0; i < m; i++)
-	{
-		int temp_id;
-		int childNum;
-		scanf_s("%d %d", &temp_id, &childNum);
-		id[temp_id][0] = 1;//表示该节点是非叶节点
-		for (int j = 0; j < childNum; j++)
-		{
-			int temp_child;
-			cin >> temp_child;
-			//child.push_back(temp_child);
-			id[temp_id][temp_child] = 1;//表示该节点是儿子
-		}
-	}
 
-	
-	system("pause");
+int main() {
+	int n, 
+		m, 
+		k, //child_number
+		node, 
+		c;
+	scanf_s("%d %d", &n, &m);
+	for (int i = 0; i < m; i++) {
+		scanf_s("%d %d", &node, &k);
+		for (int j = 0; j < k; j++) {
+			scanf_s("%d", &c);
+			v[node].push_back(c);
+		}
+	}
+	dfs(1, 0);
+	printf("%d", book[0]);
+	for (int i = 1; i <= maxdepth; i++)
+		printf(" %d", book[i]);
+	return 0;
 }
